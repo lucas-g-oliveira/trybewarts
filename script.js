25cdece15d73471474a6c7f834699f1f9879abd2
Lucas G <lucasg.devmail93@gmail.com>
2022-07-13 20:38:53 -0300

    requisito 18 -> OK

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const btn = document.querySelector("body > header > form > button");
 const email = document.querySelector('#emailInput');
 const password = document.querySelector('#passwordInput');
+const btnSubmit = document.querySelector('#submit-btn');
+const checkBox = document.querySelector('#agreement');
 
 
 
-btn.addEventListener('click',function() {
-    if(email.value=='tryber@teste.com' && password.value=='123456'){
+checkBox.addEventListener('click', () => {
+    checkBox.checked ?  (btnSubmit.disabled = false):(btnSubmit.disabled = true);
+});
+
+
+btn.addEventListener('click', function () {
+    if (email.value == 'tryber@teste.com' && password.value == '123456') {
         alert('Olá, Tryber!');
-    }else{
+    } else {
         alert("Email ou senha inválidos.");
     }
 })
-
-    
