7149bd258689a3d640642dc0e8126b31f847c0b2
Lucas G <lucasg.devmail93@gmail.com>
2022-07-13 20:50:39 -0300

    requisito 19 -> ok

--- cypress/integration/project.spec.js
+++ cypress/integration/project.spec.js
@@ -309,7 +309,7 @@ describe('Trybewarts', () => {
     });
   });
 
-  describe('18 - Faça com que o botão `Enviar` seja habilitado somente após a checkbox do requisito 16 ser selecionada', () => {
+  describe.only('18 - Faça com que o botão `Enviar` seja habilitado somente após a checkbox do requisito 16 ser selecionada', () => {
     it('O botão deve inicialmente estar desabilitado', () => {
       cy.get('button#submit-btn')
         .should('be.disabled');
